810fc2ca2c7ebcf64fc1cbfaaf34d359170c15f3
Victor <frankjnr368@yahoo.com>
2020-08-20 14:39:19 +0100

    Added functional test and using unittest

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+#from django.test import TestCase
+from selenium import webdriver
+import unittest
 
-# Create your tests here.
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Victor heard of the new cool website. He goes
+        #to check its homepage
+        self.browser.get('http://127.0.0.1:8000')
+
+        #he notices the header and page title mentions thought...
+        self.assertIn('Thoughts...', self.browser.title)
+        self.fail('Finish the test! ')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
